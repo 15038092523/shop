06dbf0b7314f370bcea5ed38002c4de0f1a84000
pxk <15038092523@163.com>
2020-05-21 00:17:51 +0800

    customer redis缓存

--- shop-customer/src/main/java/com/customer/service/impl/CustomerServiceImpl.java
+++ shop-customer/src/main/java/com/customer/service/impl/CustomerServiceImpl.java
@@ -1,9 +1,12 @@
 package com.customer.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.customer.entity.Customer;
 import com.customer.repository.CustomerRepository;
 import com.customer.service.CustomerService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,22 +17,32 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    public StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Customer create(Customer customer) {
         customer = customerRepository.save(customer);
+        String customerStr = JSON.toJSONString(customer);
+        stringRedisTemplate.opsForValue().set(customer.getId(), customerStr);
         return customer;
     }
 
     @Override
     public Customer findById(String id) {
-        Customer customer = customerRepository.findOne(id);
-        /*//String result = redisService.get(id);
-        if (!StringUtils.isBlank(result)) {
-          //  customer = JSONObject.parseObject(result, Customer.class);
+        String customerStr = stringRedisTemplate.opsForValue().get(id);
+        Customer customer = null;
+        if (!StringUtils.isBlank(customerStr)) {
+            customer = JSON.parseObject(customerStr, Customer.class);
+            return customer;
         }
         if (customer == null) {
-            //customer = customerRepository.findOne(id);
-        }*/
+            customer = customerRepository.findOne(id);
+            if (customer != null) {
+                customerStr = JSON.toJSONString(customer);
+                stringRedisTemplate.opsForValue().set(customer.getId(), customerStr);
+            }
+        }
         return customer;
 
     }
